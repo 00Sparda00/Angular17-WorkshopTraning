ab9cb106cbc93ce2d6b654c45dd1444b2461a986
Panlop <panlop.developer@gmail.com>
2024-06-07 22:38:34 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,26 +4,35 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { Component } from '@angular/core';
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component:HomeComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent    
-    },
-    {
-        path: 'contact',
-        component: ContactComponent  
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },   
-    {
-        path: 'register',
-        component: RegisterComponent    
-    }
+
+   {
+    path:'',
+    component:FrontLayoutComponent ,
+    children:[
+        {
+            path: '',
+            component:HomeComponent
+        },
+        {
+            path: 'about',
+            component: AboutComponent    
+        },
+        {
+            path: 'contact',
+            component: ContactComponent  
+        },
+        {
+            path: 'login',
+            component: LoginComponent
+        },   
+        {
+            path: 'register',
+            component: RegisterComponent    
+        }
+    ]
+}
 ];
